7c3858ffe813f2258756c9c809735113d6dc7145
Emerald <ciudadesmeraldacr@gmail.com>
2025-11-25 16:14:11 -0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, JSX } from "react";
 
 import CalendarSection from "@/app/components/sections/CalendarSection";
 import ReservationSection from "@/app/components/sections/ReservationSection";
