3d5236004f90bb39ee9707970d31edbd0d409f95
Emerald <ciudadesmeraldacr@gmail.com>
2025-11-25 20:39:56 -0600

    set

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -18,12 +18,12 @@ export function getPayPalApiBaseUrl() {
  * Uses CLIENT_ID and SECRET to create a Basic token
  */
 export function getPayPalAuthHeader() {
-  if (!process.env.PAYPAL_CLIENT_ID || !process.env.PAYPAL_CLIENT_SECRET) {
+  if (!process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || !process.env.PAYPAL_CLIENT_SECRET) {
     throw new Error("Missing PayPal API credentials.");
   }
 
   const encoded = Buffer.from(
-    `${process.env.PAYPAL_CLIENT_ID}:${process.env.PAYPAL_CLIENT_SECRET}`
+    `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}:${process.env.PAYPAL_CLIENT_SECRET}`
   ).toString("base64");
 
   return `Basic ${encoded}`;
