ec8242a6fe6f69d92f5b2b76dee283eb14ccab09
Allan Rojas <90080194+emeraldcr@users.noreply.github.com>
2026-05-03 19:10:02 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { Analytics } from "@vercel/analytics/next";
 import { LanguageProvider } from "@/app/context/LanguageContext";
 import SessionProvider from "@/app/components/SessionProvider";
 import AnalyticsTracker from "@/app/components/analytics/AnalyticsTracker";
-import GoogleAdsScript from "@/app/components/analytics/GoogleAdsScript";
 
 export const metadata: Metadata = {
   title: "La Vieja Adventures | Ciudad Esmeralda Tour Aventura San Carlos en Rio La Vieja",
@@ -50,7 +49,7 @@ export default function RootLayout({
           />
         </noscript>
 
-        <GoogleAdsScript />
+       
         
         <SessionProvider>
           <LanguageProvider>
